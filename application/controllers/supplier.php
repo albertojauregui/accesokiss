495ab1456da239785413a586cb0d48fbedb1dd3f
Aldo Utrera <Kugel85@gmail.com>
2012-07-24 11:58:22 -0500

    Commit inicial de los controladores

---
+++ application/controllers/supplier.php
@@ -0,0 +1,30 @@
+<?php
+
+class Supplier_Controller extends Base_Controller {
+
+	public function action_index()
+	{
+
+	}
+	
+	public function action_view()
+	{
+		
+	}
+	
+	public function action_add()
+	{
+		
+	}
+	
+	public function action_edit()
+	{
+		
+	}
+	
+	public function action_delete()
+	{
+		
+	}
+	
+}
