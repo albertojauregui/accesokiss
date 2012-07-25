3ba988f0e334bcb7efaf3ba9a10493dfb759eccb
Aldo Utrera <Kugel85@gmail.com>
2012-07-25 17:36:26 -0500

    Commit inicial a la carpeta que contendrá las vistas de los provedores. Commit inicial de la vista index y add de proveedores

---
+++ application/views/suppliers/add.blade.php
@@ -0,0 +1,30 @@
+@layout('layouts.common')
+
+@section('assets')
+	@parent
+@endsection
+
+@section('content')
+	<div class="suppliers-add">
+		<div class="page-header">
+			<h1>.: Agrega un proveedor</h1>
+		</div>
+		<div class="form-wrapper">
+			{{ Form::open('/suppliers/add', 'POST') }}
+				{{ Form::label('name', 'Nombre')}}
+				{{ Form::text('name') }}
+
+				{{ Form::label('url', 'URL')}}
+				{{ Form::text('url') }}
+
+				{{ Form::label('address', 'Dirección')}}
+				{{ Form::text('address') }}
+
+				{{ Form::label('phone', 'Teléfono')}}
+				{{ Form::text('phone') }}
+
+				{{ Form::submit('Agrega el Proveedor', array('class' => 'btn-primary btn-large')) }}
+			{{ Form::close() }}
+		</div>
+	</div>
+@endsection
