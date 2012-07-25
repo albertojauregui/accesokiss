8891f0ecb212025e89bfbcb5d78e8bebfa225efa
Aldo Utrera <Kugel85@gmail.com>
2012-07-25 16:00:55 -0500

    Cambiando el header, ahora está dentro del login box, incrementando el tamaño del botón de submit

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -5,9 +5,12 @@
 @endsection
 
 @section('content')
-	<h1>Acceso KISS</h1>
 	<div class="login-box">
+		<h1>Acceso KISS</h1>
 		<div class="greeting">
+			<div class="page-header">
+				<h1><small>Bienvenido</small></h1>
+			</div>
 		</div>	
 		<div class="form-wrapper">
 			{{ Form::open('/users/login', 'POST') }}
@@ -17,7 +20,7 @@
 				{{ Form::label('password', 'Contraseña')}}
 				{{ Form::password('password') }}
 
-				{{ Form::submit('Logueate a tu cuenta', array('class' => 'btn-primary')) }}
+				{{ Form::submit('Logueate a tu cuenta', array('class' => 'btn-primary btn-large')) }}
 			{{ Form::close() }}
 		</div>
 	</div>
