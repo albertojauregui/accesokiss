cda9c39e7ded9e5d5d45950b47aa769aef795a35
Aldo Utrera <Kugel85@gmail.com>
2012-07-25 17:36:26 -0500

    Commit inicial a la carpeta que contendrá las vistas de los provedores. Commit inicial de la vista index y add de proveedores

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -5,23 +5,27 @@
 @endsection
 
 @section('content')
-	<div class="login-box">
-		<h1>Acceso KISS</h1>
-		<div class="greeting">
-			<div class="page-header">
-				<h1><small>Bienvenido</small></h1>
+	<div class="row">
+		<div class="span4 offset4">
+			<h1>Acceso KISS</h1>
+			<div class="login-box">
+				<div class="greeting">
+					<div class="page-header">
+						<h1><small>Bienvenido</small></h1>
+					</div>
+				</div>	
+				<div class="form-wrapper">
+					{{ Form::open('/users/login', 'POST') }}
+						{{ Form::label('username', 'Usuario')}}
+						{{ Form::text('username') }}
+
+						{{ Form::label('password', 'Contraseña')}}
+						{{ Form::password('password') }}
+
+						{{ Form::submit('Logueate a tu cuenta', array('class' => 'btn-primary btn-large')) }}
+					{{ Form::close() }}
+				</div>
 			</div>
-		</div>	
-		<div class="form-wrapper">
-			{{ Form::open('/users/login', 'POST') }}
-				{{ Form::label('username', 'Usuario')}}
-				{{ Form::text('username') }}
-
-				{{ Form::label('password', 'Contraseña')}}
-				{{ Form::password('password') }}
-
-				{{ Form::submit('Logueate a tu cuenta', array('class' => 'btn-primary btn-large')) }}
-			{{ Form::close() }}
 		</div>
 	</div>
 @endsection
---
+++ application/views/suppliers/index.blade.php
@@ -0,0 +1,15 @@
+@layout('layouts.common')
+
+@section('assets')
+	@parent
+@endsection
+
+@section('content')
+	<div class="suppliers-index">
+		<div class="page-header">
+			<h1>.: Proveedores</h1>
+		</div>
+		<div class="suppliers-list">
+		</div>
+	</div>
+@endsection
