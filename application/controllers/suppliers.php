b0efaafa5390103b3e15c5623d11b5c71b66c57c
Aldo Utrera <Kugel85@gmail.com>
2012-07-25 17:10:40 -0500

    Agregando la lógica del index y del agregado de proveedores#

--- application/controllers/suppliers.php
+++ application/controllers/suppliers.php
@@ -4,17 +4,35 @@ class Suppliers_Controller extends Base_Controller {
 
 	public function action_index()
 	{
-
+		return View::make('suppliers.index');
 	}
 	
-	public function action_view()
+	public function action_view($id)
 	{
-		
+		$supplier = Supplier::find($id);	
+		return View::make('suppliers.view', $supplier);
 	}
 	
 	public function action_add()
 	{
-		
+		if (Request::method() == 'GET'){
+			return View::make('suppliers.add');
+		} elseif (Request::method() == 'POST') {
+			$supplier = new Supplier;
+			$supplier->name    = Input::get('name');
+			$supplier->url     = Input::get('url');
+			$supplier->address = Input::get('address');
+			$supplier->phone   = Input::get('phone');
+			if ($supplier->save()){
+				// Guardado con éxito
+				Session::flash('status', 'Guardado satisfactorio.');
+				return Redirect::to('/suppliers');
+			} else {
+				// Guardado fallido
+				Session::flash('status', 'Guardado fallido.');
+				return Redirect::to('/suppliers/add');
+			}
+		}
 	}
 	
 	public function action_edit()
