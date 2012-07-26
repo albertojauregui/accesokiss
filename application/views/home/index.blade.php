c5adcb697002bc7f3a97a720f47214e8491ad977
Aldo Utrera <Kugel85@gmail.com>
2012-07-25 21:31:10 -0500

    Agregando clases a los input

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -6,25 +6,27 @@
 
 @section('content')
 	<div class="row">
-		<div class="span4 offset4">
-			<div class="login-box">
-				<div class="greeting">
-					<legend>Acceso KISS</legend>
-				</div>	
-				<div class="form-wrapper">
-					{{ Form::open('/users/login', 'POST', array('class' => 'form-horizontal')) }}
-						<fieldset>
-								{{ Form::label('username', 'Usuario') }}
-								{{ Form::text('username') }}
+		<div class="wrapper">
+			<div class="span4 offset4">
+				<div class="login-box">
+					<div class="greeting">
+						<legend>Acceso KISS</legend>
+					</div>	
+					<div class="form-wrapper">
+						{{ Form::open('/users/login', 'POST', array('class' => 'form-horizontal')) }}
+							<fieldset>
+									{{ Form::label('username', 'Usuario') }}
+									{{ Form::text('username', array('class' => 'span4')) }}
 
-								{{ Form::label('password', 'Contraseña') }}
-								{{ Form::password('password') }}
+									{{ Form::label('password', 'Contraseña') }}
+									{{ Form::password('password', array('class' => 'span4')) }}
 
-								<div class="row">
-									{{ Form::submit('Logueate a tu cuenta', array('class' => 'btn-primary btn-large span4')) }}
-								</div>
-						</fieldset>
-					{{ Form::close() }}
+									<div class="row">
+										{{ Form::submit('Logueate a tu cuenta', array('class' => 'btn-primary btn-large span4')) }}
+									</div>
+							</fieldset>
+						{{ Form::close() }}
+					</div>
 				</div>
 			</div>
 		</div>
