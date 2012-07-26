f33da549e9e4e739722038a6891827beeee1c3a3
Aldo Utrera <Kugel85@gmail.com>
2012-07-26 11:11:37 -0500

    Commit inicial del form para agregar usuarios

---
+++ application/views/users/add.blade.php
@@ -0,0 +1,31 @@
+@layout('layouts.common')
+
+@section('assets')
+	@parent
+@endsection
+
+@section('content')
+	<div class="users-add container-box">
+		<div class="page-header">
+			<h1>.: Agrega un usuario</h1>
+		</div>
+		<div class="row-fluid">
+			<div class="span4 offset4">
+				<div class="form-wrapper">
+					{{ Form::open('/users/add', 'POST') }}
+						{{ Form::label('name', 'Nombre de Usuario')}}
+						{{ Form::text('name') }}
+
+						{{ Form::label('password', 'Contraseña')}}
+						{{ Form::text('password') }}
+
+						{{ Form::label('is_admin', '¿Es admin?')}}
+						{{ Form::checkbox('is_admin', 1) }}
+
+						{{ Form::submit('Agrega el Proveedor', array('class' => 'btn-primary btn-large')) }}
+					{{ Form::close() }}
+				</div>
+			</div>	
+		</div>
+	</div>
+@endsection
