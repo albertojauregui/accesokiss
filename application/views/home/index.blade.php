a186ea7a1d452a8096d2e762f570199fe72aba7b
Aldo Utrera <Kugel85@gmail.com>
2012-07-26 16:16:51 -0500

    Cambiando el titulo de la página

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Laravel: A Framework For Web Artisans</title>
+	<title>Samsara: Acceso KISS</title>
 	<meta name="viewport" content="width=device-width">
 	{{ HTML::style('laravel/css/style.css') }}
 	{{ Asset::container('bootstrapper')->styles() }}
