db1dfb1003b08486782c3f720d79364dbc41a00f
Aldo Utrera <Kugel85@gmail.com>
2012-07-26 13:14:23 -0500

    Agregando el menu al layout

--- application/views/layouts/common.blade.php
+++ application/views/layouts/common.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Laravel: A Framework For Web Artisans</title>
+	<title>Samsara: Acceso KISS</title>
 	<meta name="viewport" content="width=device-width">
 	@section('assests')
 		{{ HTML::style('laravel/css/style.css') }}
@@ -12,6 +12,30 @@
 	@yield_section
 </head>
 <body>
+	<div class="container">
+		<div class="row-fluid">
+			<div class="span12">
+				<div class="btn-group menu">
+					<a href="/suppliers" class = "btn btn-large btn-inverse">
+						<i class="icon-map-marker icon-white"></i>
+						Proveedores
+					</a>
+					<a href="/brands" class = "btn btn-large btn-inverse">
+						<i class="icon-tags icon-white"></i>
+						Marcas
+					</a>
+					<a href="/users" class = "btn btn-large btn-inverse">
+						<i class="icon-user icon-white"></i>
+						Usuarios
+					</a>
+					<a href="/users/logout" class = "btn btn-large btn-inverse">
+						<i class="icon-remove icon-white"></i>
+						Salir
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
 	<div class="container">
 		@yield('content')
 	</div>
