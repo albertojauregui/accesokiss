0df14bbfd9e148a9b0d78eb9d3720e993ac10351
Aldo Utrera <Kugel85@gmail.com>
2012-07-26 10:25:21 -0500

    Agregando markup para presentar mejor el formulario de agregar

--- application/views/suppliers/add.blade.php
+++ application/views/suppliers/add.blade.php
@@ -5,26 +5,30 @@
 @endsection
 
 @section('content')
-	<div class="suppliers-add">
+	<div class="suppliers-add container-box">
 		<div class="page-header">
 			<h1>.: Agrega un proveedor</h1>
 		</div>
-		<div class="form-wrapper">
-			{{ Form::open('/suppliers/add', 'POST') }}
-				{{ Form::label('name', 'Nombre')}}
-				{{ Form::text('name') }}
+		<div class="row">
+			<div class="span4 offset4">
+				<div class="form-wrapper">
+					{{ Form::open('/suppliers/add', 'POST') }}
+						{{ Form::label('name', 'Nombre')}}
+						{{ Form::text('name') }}
 
-				{{ Form::label('url', 'URL')}}
-				{{ Form::text('url') }}
+						{{ Form::label('url', 'URL')}}
+						{{ Form::text('url') }}
 
-				{{ Form::label('address', 'Dirección')}}
-				{{ Form::text('address') }}
+						{{ Form::label('address', 'Dirección')}}
+						{{ Form::text('address') }}
 
-				{{ Form::label('phone', 'Teléfono')}}
-				{{ Form::text('phone') }}
+						{{ Form::label('phone', 'Teléfono')}}
+						{{ Form::text('phone') }}
 
-				{{ Form::submit('Agrega el Proveedor', array('class' => 'btn-primary btn-large')) }}
-			{{ Form::close() }}
+						{{ Form::submit('Agrega el Proveedor', array('class' => 'btn-primary btn-large')) }}
+					{{ Form::close() }}
+				</div>
+			</div>	
 		</div>
 	</div>
 @endsection
