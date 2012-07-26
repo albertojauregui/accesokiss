cc3ef01f3f610b1214332aaf73f772b243682767
Aldo Utrera <Kugel85@gmail.com>
2012-07-25 22:04:52 -0500

    Removiendo el uso de las clase Form para los inputs

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -16,13 +16,9 @@
 						<div class="form-wrapper">
 							{{ Form::open('/users/login', 'POST', array('class' => 'form-horizontal')) }}
 								<fieldset>
-										{{ Form::label('username', 'Usuario') }}
-										{{ Form::text('username') }}
-
-										{{ Form::label('password', 'Contraseña') }}
-										{{ Form::password('password') }}
-
-										{{ Form::submit('Logueate a tu cuenta', array('class' => 'btn-primary btn-large span12')) }}
+									<input type="text" placeholder="Usuario" name = "username" class = "span12">
+									<input type="password" placeholder="Contraseña" name = "password" class = "span12">
+									{{ Form::submit('Logueate a tu cuenta', array('class' => 'btn-primary btn-large span12')) }}
 								</fieldset>
 							{{ Form::close() }}
 						</div>
