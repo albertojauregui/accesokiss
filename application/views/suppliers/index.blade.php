455ae79be360c9bb581c78c43e12ae3b1b345201
Aldo Utrera <Kugel85@gmail.com>
2012-07-26 21:47:54 -0500

    Agregando markup para el indexado de proveedores

--- application/views/suppliers/index.blade.php
+++ application/views/suppliers/index.blade.php
@@ -10,18 +10,52 @@
 			<h1>.: Proveedores</h1>
 		</div>
 		<div class="suppliers-list">
-			<div class="row">
-				<div class="span10 offset1">
+			<div class="row-fluid">
+				<div class="span12">
 					<div class="list-header">
-						<div class="name">
-						</div>
-						<div class="url">
-						</div>
-						<div class="address">
-						</div>
-						<div class="phone">
+						<div class="row-fluid">
+							<div class="span3">
+								Nombre
+							</div>
+							<div class="span3">
+								Página Web
+							</div>
+							<div class="span2">
+								Dirección
+							</div>
+							<div class="span2">
+								Teléfono
+							</div>
+							<div class="span2">
+								Acciones
+							</div>
 						</div>
 					</div>
+					<div class="list-element">
+						<div class="row-fluid">
+							<div class="span3">
+								Radio antenas del Cariba S.A. de C.V.
+							</div>
+							<div class="span3">
+								http://www.mongeeks.com.mx
+							</div>
+							<div class="span2">
+								Av. Lombardo Soberano #18 Catapulta 3
+							</div>
+							<div class="span2">
+								9985772496
+							</div>
+							<div class="span2">
+								<a href = "#" class = "btn btn-warning" rel = "tooltip" title = "Editar el proveedor">
+									<i class="icon-pencil icon-white"></i>
+								</a>
+								<a href = "#" class = "btn btn-danger" rel = "tooltip" title = "Eliminar el proveedor">
+									<i class="icon-remove-sign icon-white"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+					<hr>
 					<div class="list-element">
 					</div>
 				</div>
