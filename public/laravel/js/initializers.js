7d346b941fb34e6d4e67f7d13353cb655632e843
Aldo Utrera <Kugel85@gmail.com>
2012-07-27 13:05:14 -0500

    Agregamos la función que centrará el menú y agregamos un handler para llamarlo cada que se modifique el tamaño de la ventana

--- public/laravel/js/initializers.js
+++ public/laravel/js/initializers.js
@@ -1,5 +1,20 @@
 $(function (){
 
 	$('a[rel=tooltip]').tooltip();
+
+	resizeMenu('.btn-group.menu', '.btn-group.menu .btn');
+
+	$(window).resize(function (){
+		resizeMenu('.btn-group.menu', '.btn-group.menu .btn');
+	});
 	
 });
+
+function resizeMenu(selectorMenu, selectorMenuItems){
+	var menuItems = $(selectorMenuItems).length;
+	var width     = $(selectorMenu).width();
+	var size      = width / menuItems - 2;
+	$.each($(selectorMenuItems), function(){
+		$(this).css('width', size);
+	});
+}
