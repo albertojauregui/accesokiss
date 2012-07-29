586cfa372cbbee6a1b94310b9ed5d615e4728ef0
Aldo Utrera <Kugel85@gmail.com>
2012-07-29 00:46:03 -0500

    Agregando campos para la tabla pivote

--- application/models/User.php
+++ application/models/User.php
@@ -4,7 +4,7 @@ class User extends Eloquent {
 	
 	public function suppliers()
 	{
-		return $this->has_many_and_belongs_to('Supplier');
+		return $this->has_many_and_belongs_to('Supplier')->with('user', 'password');
 	}
 
 }
