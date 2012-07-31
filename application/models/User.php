b5982ff54f649686bddf45532b204cea6ae05526
Aldo Utrera <Kugel85@gmail.com>
2012-07-30 20:20:19 -0500

    Ordenando las tablas relacionadas alfabeticamente

--- application/models/User.php
+++ application/models/User.php
@@ -4,7 +4,7 @@ class User extends Eloquent {
 	
 	public function suppliers()
 	{
-		return $this->has_many_and_belongs_to('Supplier')->with('user', 'password');
+		return $this->has_many_and_belongs_to('Supplier')->with('user', 'password')->order_by('name', 'ASC');
 	}
 
 }
