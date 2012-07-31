caad447025d3f04c9c6e3a9e5635cd57a506b7c7
Aldo Utrera <Kugel85@gmail.com>
2012-07-30 20:20:00 -0500

    Ordenando las tablas relacionadas alfabeticamente

--- application/models/Supplier.php
+++ application/models/Supplier.php
@@ -4,12 +4,14 @@ class Supplier extends Eloquent {
 	
 	public function users()
 	{
-		return $this->has_many_and_belongs_to('User');
+		return $this->has_many_and_belongs_to('User')
+			->order_by('name', 'ASC');
 	}
 
 	public function brands()
 	{
-		return $this->has_many_and_belongs_to('Brand');
+		return $this->has_many_and_belongs_to('Brand')
+			->order_by('name', 'ASC');
 	}
 
 }
